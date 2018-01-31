5e5990cbb9c146efd2d19e37fb166f06601f9b81
jbrown53 <jbrown53@seattlecentral.edu>
2018-01-31 11:45:50 -0800

    Add project files.

---
+++ CommunityAssist/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CommunityAssist
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CommunityAssist/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CommunityAssist.MvcApplication" Language="C#" %>
