cf41634b4763c161c470ddc5b982de37e9ee613f
jbrown53 <jbrown53@seattlecentral.edu>
2018-01-31 12:16:43 -0800

    Grant Types

--- CommunityAssist/Controllers/HomeController.cs
+++ CommunityAssist/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CommunityAssist.Models;
 
 namespace CommunityAssist.Controllers
 {
@@ -10,7 +11,8 @@ namespace CommunityAssist.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+            return View(db.GrantTypes.ToList());
         }
 
         public ActionResult About()
